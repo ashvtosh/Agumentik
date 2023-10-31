10d47acf12ebcee01395b648d8f454b8b3cc4d8b
Ashutosh <ashutosh1412003@gmail.com>
2023-11-01 02:42:26 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import './main.css';
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-content">
+        <div className='fitsr1'>
+            <h2><span style={{fontWeight:"bolder", color:"orange"}}>monday</span>.com</h2>
+            <br></br>
+            <p className='p89'>Pricing</p>
+            <p className='p89'>Contact us</p>
+            <p className='p89'>Templates</p>
+            <p className='p89'>SMB</p>
+            <p className='p89'>Enterprise</p>
+            <p className='p89'>Nonprofits</p>
+            <p className='p89'>App marketplace</p>
+            <p className='p89'>24/7 support</p>
+        </div>
+        <div className='second2'>
+            <h5>Features</h5>
+            <br></br>
+            <p className='p89'>Docs</p>
+            <p className='p89'>integrations</p>
+            <p className='p89'>Automation</p>
+            <p className='p89'>Files</p>
+            <p className='p89'>Dashboard</p>
+            <p className='p89'>Kanban</p>
+            <p className='p89'>Gant</p>
+        </div>
+        <div className='third3'>
+            <h5>monday products</h5>
+            <br></br>
+            <p className='p89'>monday work manager</p>
+            <p className='p89'>monday sales CRM</p>
+            <p className='p89'>monday dev</p>
+            <br></br>
+            <h5>more by monday.com</h5>
+            <p className='p89'>WorkCanvas</p>
+            <p className='p89'>WorkForms</p>
+        </div>
+        <div className='fourth4'>
+            <h5>Use Cases</h5>
+            <br></br>
+            <p className='p89'>Marketing</p>
+            <p className='p89'>Project Management</p>
+            <p className='p89'>Sales</p>
+            <p className='p89'>Developers</p>
+            <p className='p89'>HR</p>
+            <p className='p89'>IT</p>
+            <p className='p89'>Operations</p>
+        </div>
+        <div className='fourth'>
+            <h5>Company</h5>
+            <br></br>
+            <p className='p89'>Docs</p>
+            <p className='p89'>integrations</p>
+            <p className='p89'>Automation</p>
+            <p className='p89'>Files</p>
+            <p className='p89'>Dashboard</p>
+            <p className='p89'>Kanban</p>
+            <p className='p89'>Gant</p>
+            <p className='p89'>WorkForms</p>
+            <p className='p89'>WorkForms</p>
+        </div>
+        <div className='sixth6'>
+            <h5>Resources</h5>
+            <br></br>
+            <p className='p89'>Docs</p>
+            <p className='p89'>integrations</p>
+            <p className='p89'>Automation</p>
+            <p className='p89'>Files</p>
+            <p className='p89'>Dashboard</p>
+            <p className='p89'>Kanban</p>
+            <p className='p89'>Gant</p>
+            <p className='p89'>WorkForms</p>
+            <p className='p89'>WorkForms</p>
+            <p className='p89'>WorkForms</p>
+            <p className='p89'>WorkForms</p>
+        </div>
+      </div>
+
+      <div className='lowerfoot'>
+          <p style={{marginLeft:"15px", marginTop:"30px"}}>English</p>
+          <div className='social media'>
+
+          </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
